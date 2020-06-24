6784ce3d053517f5b20c4eb43688c0ce994321bd
Jonathan Wilson <wilsonwjonathan@gmail.com>
2020-06-24 09:30:43 -0700

    updated workflows

--- .github/actions/s3_sync/entrypoint.sh
+++ .github/actions/s3_sync/entrypoint.sh
@@ -7,5 +7,6 @@ touch ~/.aws/config
 echo '[profile crossaccount]
 
 role_arn='$RUNNER_ROLE'
+external_id='$RUNNER_EXTERNAL_ID'
 credential_source=Ec2InstanceMetadata' > ~/.aws/config
 aws s3 --profile crossaccount sync --acl public-read _site s3://$BUCKET_NAME --delete
