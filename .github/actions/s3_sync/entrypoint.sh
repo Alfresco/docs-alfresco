22ed141cb744383ae3cd9eaa6a745dfd067bdf27
Jonathan Wilson <wilsonwjonathan@gmail.com>
2020-06-25 11:32:11 -0700

    invalidating cloudfront cache on deploy

--- .github/actions/s3_sync/entrypoint.sh
+++ .github/actions/s3_sync/entrypoint.sh
@@ -10,3 +10,4 @@ role_arn='$RUNNER_ROLE'
 external_id='$RUNNER_EXTERNAL_ID'
 credential_source=Ec2InstanceMetadata' > ~/.aws/config
 aws s3 --profile crossaccount sync --acl public-read _site s3://$BUCKET_NAME --delete
+aws cloudfront --profile crossaccount create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
