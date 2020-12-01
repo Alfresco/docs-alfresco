b7e570c26c39634ebce04b483988100a4abce62f
Jonathan Wilson <wilsonwjonathan@gmail.com>
2020-12-01 06:40:30 -0800

    Redirects (#82)
    * added redirects S3 upload action
    
    * note Elasticsearch settigns
    
    * fixed method not defined error in s3 upload action
    
    * upload redirects with public access
    
    * redirecting to relative urls
    
    * checking for absolute urls in redirects
    
    * changed redirect absolute urls to relative urls
    
    * removed .html from redirect urls
    
    * exclude redirects/ from jekyll build
    
    * exclude README from jekyll build

--- .github/actions/s3_sync/entrypoint.sh
+++ .github/actions/s3_sync/entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-mkdir ~/.aws
-touch ~/.aws/config
-echo '[profile crossaccount]
-
-role_arn='$RUNNER_ROLE'
-external_id='$RUNNER_EXTERNAL_ID'
-credential_source=Ec2InstanceMetadata' > ~/.aws/config
 aws s3 --profile crossaccount sync --acl public-read _site s3://$BUCKET_NAME --delete
 aws cloudfront --profile crossaccount create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
 
